7deaac89bb9f83d3aadf2fc6d8a3a985d30dd438
Juggoz10 <juggoz10@gmail.com>
2023-09-26 19:06:54 -0500

    primer cargue

---
+++ switch.php
@@ -0,0 +1,13 @@
+<?php
+$dia = "miercoles";
+switch ($dia) {
+    case "lunes":
+        echo "Hoy es lunes.";
+        break;
+    case "martes":
+        echo "Hoy es martes.";
+        break;
+    default:
+        echo "Hoy no es ni lunes ni martes.";
+}
+?>
